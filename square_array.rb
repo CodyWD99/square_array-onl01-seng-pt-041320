80ff5e6e6f5d2850e111ef46486850e8b5596bef
Cody Dupuis <codydupuis99@gmail.com>
2020-04-20 11:59:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
-def square_array(array)
-  squared_array = []
-  array.each do |num|
-    square = num ** 2
-    squared_array << square
-  end
-  squared_array
- end 
+def square_array(numbers)
+  numbers = []
+  numbers.each do |num|
+    sqrdnum = num**2
+    >>
